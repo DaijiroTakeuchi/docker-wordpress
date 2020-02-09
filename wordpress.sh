67c718b3cd44235ce90ede9b99707405640ae94d
Daijiro Takeuchi <mr.daijiro0517@gmail.com>
2020-02-09 19:25:13 +0900

    update write style for wordpress

--- wordpress.sh
+++ wordpress.sh
@@ -11,7 +11,16 @@ touch wp-content/themes/$str/functions.php
 touch wp-content/themes/$str/header.php
 touch wp-content/themes/$str/footer.php
 touch wp-content/themes/$str/sidebar.php
+touch wp-content/themes/$str/sidebar-archives.php
+touch wp-content/themes/$str/sidebar-categories.php
+touch wp-content/themes/$str/archives.php
 touch wp-content/themes/$str/index.php
 touch wp-content/themes/$str/home.php
 touch wp-content/themes/$str/single.php
 touch wp-content/themes/$str/page.php
+touch wp-content/themes/$str/front-page.php
+touch wp-content/themes/$str/loop-main.php
+touch wp-content/themes/$str/search.php
+mkdir wp-content/themes/$str/images
+mkdir wp-content/themes/$str/images-content
+mkdir wp-content/themes/$str/scripts
